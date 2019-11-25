0b872bfcccefe730de611ad70c2e532112f360c0
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-24 21:23:59 -0800

    working on Model Objects Methods

--- demo/urls.py
+++ demo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import Another
 
 urlpatterns = [
-    path('firstfunction', views.first),
+    path('first', views.first),
+    path('another', Another.as_view())
 
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
-# Create your views here.
+
+class Another(View):
+
+    books = Book.objects.all()
+
+    output = f"We have {len(books)} that many books in DB"
+
+    def get(self, request):
+        return HttpResponse(self.output)
+
+
 def first(request):
     return HttpResponse('First message from views')
