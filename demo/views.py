1aacdc712326719a7a19798aa87c3a6ce09b3ea4
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-25 13:21:13 -0800

    Changing views for html

--- demo/views.py
+++ demo/views.py
@@ -6,11 +6,11 @@ from .models import Book
 
 class Another(View):
 
-    books = Book.objects.filter
-    output = ''
+    book = Book.objects.get(id=2)
+    output = f"We have {book.title} book with ID {book.id}<br>"
 
-    for book in books:
-        output += f"We have {book.title} books in ID {book.id}<br>"
+    # for book in books:
+    #     output += f"We have {book.title} books in ID {book.id}<br>"
 
     def get(self, request):
         return HttpResponse(self.output)
