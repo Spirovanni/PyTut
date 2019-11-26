41c9538bb96034bab871ceeb4aa2110c3ccd272e
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-25 21:40:13 -0800

    Begin work with serializers and API's

--- demo/views.py
+++ demo/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
+from rest_framework import viewsets
+from .serializers import BookSerializer
 from .models import Book
-from django.shortcuts import render
 
-
-def first(request):
-    books = Book.objects.all()
-
-    return render(request, 'first_temp.html', {'books': books})
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
