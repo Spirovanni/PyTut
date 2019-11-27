79a7bf43c01a884b0787798561a0aad1b348a7b9
Xavier Martinez <blackshieldsx@gmail.com>
2019-11-26 19:51:29 -0800

    Working with serializers

--- demo/serializers.py
+++ demo/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title']
+        fields = ['title', 'description', 'price']
